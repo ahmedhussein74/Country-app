0a01717c5561cc38f6cabd54631a236f000a7450
ahmedhussein74 <ahmedhussein.2072@gmail.com>
2022-07-24 16:15:30 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React , { useState , useEffect } from 'react'
-import './App.css';
+import './App.css'
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState({firstname:'', lastname:''})
 
   useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
+    document.title = name.firstname;
+  }, [name.firstname])
 
   return (
     <div className="App">
-      <button onClick={ () => { setCount (count - 1) }}>-</button>
-      <button onClick={ () => { setCount (0) }}>Reset</button>
-      <button onClick={ () => { setCount (count + 1) }}>+</button>
-      <h3>you click me {count} times</h3>
+      <input onChange={e => setName({...name, firstname: e.target.value})} placeholder="Enter Your First Name" />
+      <input onChange={e => setName({...name, lastname: e.target.value})} placeholder="Enter Your Last Name" />
+      <h3>First Name : {name.firstname}</h3>
+      <h3>Last Name : {name.lastname}</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
